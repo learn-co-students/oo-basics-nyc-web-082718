d092a2bd34fa8564f581afef9fc1956a9e9c0a26
yammik <mkim0514@gmail.com>
2018-08-28 14:25:14 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  attr_accessor :author, :genre, :page_count
 
+  def title
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
