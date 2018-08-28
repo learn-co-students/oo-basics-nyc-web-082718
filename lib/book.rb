41908729c3d462aa00b8cccceb69f945edcedd47
yammik <mkim0514@gmail.com>
2018-08-28 14:25:46 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,9 @@
 class Book
+  attr_accessor :author, :genre, :page_count
+
   def initialize(title)
     @title = title
   end
-
-  attr_accessor :author, :genre, :page_count
 
   def title
     @title
